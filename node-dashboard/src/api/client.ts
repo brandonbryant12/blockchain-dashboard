9b227221959a3b9b44778299a61dbfb585133b33
Brandon Bryant <brandon.bryant002@gmail.com>
2024-09-28 20:01:59 -0400

    test

--- node-dashboard/src/api/client.ts
+++ node-dashboard/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: "https://nodemaster.online/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true, // If you need to send cookies
 });
 
